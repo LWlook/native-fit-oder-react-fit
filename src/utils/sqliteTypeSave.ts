e61c5e3439c22d41e5e3b51df2fab53a6d8b49a4
Michael Leister <michael@we-dev.at>
2021-04-01 11:38:36 +0200

    Typesave example

--- src/utils/sqliteTypeSave.ts
+++ src/utils/sqliteTypeSave.ts
@@ -1,12 +1,13 @@
 import {db} from "../App";
+import {SQLResultSet} from "expo-sqlite";
 
 export const fetchTypeSaveSql = async <T>(sqlStatement: string, args: any[] | undefined): Promise<T[]> => {
     return new Promise((resolve) => {
         db.transaction(tx => {
             tx.executeSql(
                 sqlStatement, args,
-                (_, result) => {
-                    resolve(Array.from(result.rows as any) as T[])
+                (_, result: SQLResultSet) => {
+                    resolve(Object.values(result.rows) as T[])
                 },
                 (_, error): boolean => {
                     console.warn(error)
