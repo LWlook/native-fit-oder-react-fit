366d7889783723efc841653c84f71d13c6478a05
Michael Leister <michael@we-dev.at>
2021-04-01 11:50:33 +0200

    Add Inputs for ChoseExercise Screen

---
+++ src/components/ExerciseInput.tsx
@@ -0,0 +1,77 @@
+import {StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {FontAwesome} from "@expo/vector-icons";
+import React from "react";
+import {colors} from "../constants/style";
+
+interface ExerciseInputProps {
+    title: string
+    value: number
+    stepSize: number
+    onChangeValue: React.Dispatch<React.SetStateAction<number>>
+
+}
+
+export const ExerciseInput: React.FC<ExerciseInputProps> = ({ title, onChangeValue, stepSize, value}) => {
+
+    const reduceValue = () => {
+        onChangeValue((prevValue) => {
+            let newValue = prevValue - stepSize
+            if (newValue < 0) newValue = 0
+            return newValue
+        })
+    }
+
+    const increaseValue = () => {
+        onChangeValue((prevValue) => {
+            return prevValue + stepSize
+        })
+    }
+
+    const changeValue = (value: string) => {
+        const numericValue = parseInt(value.replace(/[^0-9]/g, ''))
+        if (isNaN(numericValue)) return;
+        onChangeValue(numericValue)
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{ title }</Text>
+            <View style={styles.inputContainer}>
+                <TouchableOpacity style={styles.button} onPress={reduceValue}>
+                    <FontAwesome name="minus" size={24} color="black"/>
+                </TouchableOpacity>
+                <TextInput style={styles.input} value={value.toString()} onChangeText={changeValue} keyboardType="numeric"/>
+                <TouchableOpacity style={styles.button} onPress={increaseValue}>
+                    <FontAwesome name="plus" size={24} color="black"/>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: "bold",
+        marginBottom: 8
+    },
+    input: {
+        borderBottomWidth: 1,
+        marginHorizontal: 8,
+        padding: 4
+    },
+    inputContainer: {
+        flex: 1,
+        flexDirection: "row"
+    },
+    button: {
+        borderWidth: 1,
+        borderColor: colors.black,
+        borderRadius: 4,
+        padding: 8
+    }
+});
--- src/screens/ChoseExercise.tsx
+++ src/screens/ChoseExercise.tsx
@@ -1,15 +1,23 @@
-import React from "react";
-import {Text, View} from "react-native";
+import React, {useState} from "react";
+import {StyleSheet, View} from "react-native";
 import {HomeStackParamList} from "../navigators/HomeNavigator";
 import {RouteProp, useRoute} from "@react-navigation/native";
+import {ExerciseInput} from "../components/ExerciseInput";
 
 export const ChoseExercise: React.FC = () => {
     const route = useRoute<RouteProp<HomeStackParamList, 'ChoseExercise'>>()
     const {exerciseId} = route.params
+    const [weight, setWeight] = useState<number>(0)
+    const [reps, setReps] = useState<number>(0)
 
-    return <View>
-        <Text>
-            ModifyExercise Screen! (ID: {exerciseId})
-        </Text>
+    return <View style={styles.container}>
+        <ExerciseInput title="Weight" value={weight} onChangeValue={setWeight} stepSize={2.5}/>
+        <ExerciseInput title="Reps" value={reps} onChangeValue={setReps} stepSize={1}/>
     </View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center"
+    }
+});
