712608accdc4fc9962a0e95e10a79cd2ce13dabb
Michael Leister <michael@we-dev.at>
2021-04-13 09:40:33 +0200

    Add images & colors

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -4,6 +4,9 @@ export const colors = {
     primary: "#3fb984",
     secondary: "#31475e",
     black: "#000",
+    grey: '#bdc3c7',
+    increase: '#3fb984',
+    decrease: '#e74c3c'
 };
 
 export const exerciseCategoryColors = {
@@ -14,4 +17,14 @@ export const exerciseCategoryColors = {
     back: "#f1c40f",
     legs: "#e67e22",
     abs: "#e74c3c"
+}
+
+export const exerciseCategoryImages = {
+    shoulders: require("./../assets/images/shoulders.png"),
+    triceps: require("./../assets/images/triceps.png"),
+    biceps: require("./../assets/images/biceps.png"),
+    chest: require("./../assets/images/chest.png"),
+    back: require("./../assets/images/back.png"),
+    legs: require("./../assets/images/legs.png"),
+    abs: require("./../assets/images/abs.png"),
 }
