f73435daa91446dd31be4bf6dfdc2d895c878185
Michael Leister <michael@we-dev.at>
2021-05-14 19:53:22 +0200

    Feedback: LiftedWeightCard.tsx

--- src/components/LiftedWeightCard.tsx
+++ src/components/LiftedWeightCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import {colors} from "../constants/style";
-import {MaterialCommunityIcons} from '@expo/vector-icons';
 
 interface LiftedWeightCardProps {
     value: number
@@ -11,16 +10,25 @@ export const LiftedWeightCard: React.FC<LiftedWeightCardProps> = ({value}) => {
     if (value == 0) return null
     return (
         <View style={styles.card}>
-            <MaterialCommunityIcons name="dumbbell" size={24} color="white" />
-            <Text style={styles.text}>{value} kgs lifted</Text>
-            <MaterialCommunityIcons name="dumbbell" size={24} color="white" />
+            <Image style={styles.bicepsLeft} source={require('./../assets/images/biceps.png')}/>
+            <Text style={styles.text}>{value} KGs lifted</Text>
+            <Image style={styles.bicepsRight} source={require('./../assets/images/biceps.png')}/>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    bicepsLeft: {
+        height: 24,
+        width: 24,
+        transform: [{rotateY: '180deg'}]
+    },
+    bicepsRight: {
+        height: 24,
+        width: 24,
+    },
     text: {
-      fontWeight: "bold",
+        fontWeight: "bold",
         color: colors.white,
         fontSize: 18
     },
@@ -32,7 +40,7 @@ const styles = StyleSheet.create({
         marginHorizontal: 4,
         height: "6.5%",
         shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
+        shadowOffset: {width: 0, height: 2},
         shadowRadius: 4,
         shadowOpacity: 0.26,
         elevation: 4,
